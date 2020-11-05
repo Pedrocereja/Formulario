c031db8ac8a728397ff5d5b7d397ca7ea0a28ae8
Pedrocereja <pedro_cardoso_c@hotmail.com>
2020-11-04 21:06:02 -0300

    Primeiro commit do projeto

---
+++ formulario.py
@@ -0,0 +1,99 @@
+"""
+Formulário 2 de ondas eletromagnéticas
+
+Funções
+-------
+y(R, L, G, C, w) : Constante de propagação
+Zo(R, L, G, C, w) : Impendância característica
+Zin(Zl, Zo, B, l, a=0) : Impedância de entrada
+Coef(Zl, Zo) : Coefs. de reflexão, transmissão e onda estacionária
+"""
+
+import cmath
+		
+def y(R, L, G, C, w):
+	"""
+	Constante de propagação
+	
+	Parâmetros
+	----------
+	R : Resistência
+	L : Indutância
+	G : Condutância
+	C : Capacitância
+	w : Frequência ângular(rad/s)
+	
+	Retorna a constante no formato cartesiano (alfa + jBeta)
+	"""
+	
+	x=(R+w*L*1j)*(G+w*C*1j)
+	x=cmath.sqrt(x)
+	print("Constante de atenuação (alfa): {0}".format(x.real))
+	print("Constante de fase (beta): {0}".format(x.imag))
+	return x
+
+def Zo(R, L, G, C, w):
+	"""
+	Impedância característica
+	
+	Parâmetros
+	----------
+	R : Resistência
+	L : Indutância
+	G : Condutância
+	C : Capacitância
+	w : Frequência ângular(rad/s)
+	
+	Retorna a impedância no formato cartesiano (alfa + jBeta)
+	"""
+	
+	x = (R+w*L*1j)/(G+w*C*1j)
+	x = cmath.sqrt(x)
+	
+	print("Impedância característica : {0}".format(x))
+	return x
+
+def Zin(Zl, Zo, B, l, a=0):
+	"""
+	Impedância de entrada (Meios com ou sem perdas)
+	
+	Obs : Para meios sem perdas, o valor de a é definido em 0
+	e pode ser deixado em branco
+	
+	Parâmetros
+	----------
+	Zl : Imp. característica do meio 2
+	Zo : Imp. característica do meio 1
+	B : Constante de fase da onda
+	l : Distância do ponto de encontro entre os dois
+	meios e o ponto de visão relativo da impedância
+	a : Constante de atenuação da onda
+	
+	Retorna a impedância de entrada no formato cartesiano (alfa + jBeta)
+	"""
+	
+	z = a +B*1j
+	x = cmath.atanh(z*l)
+	x = Zo*(Zl+Zo*x)/(Zo+Zl*x)
+	
+	print("Impedância de entrada : {0}".format(x))
+	return x
+
+def Coef(Zl, Zo):
+	"""
+	Calcula os coeficientes do meio, imprimindo os coefs. de reflexão, transmissão e onda estacionária
+	
+	Parâmetros
+	----------
+	Zl : Imp. característica do meio 2
+	Zo : Imp. característica do meio 1
+	"""
+	
+	TAU = (Zl - Zo)/(Zl + Zo)
+	tau = TAU + 1
+	VSWR = (1+abs(TAU))/(1-abs(TAU))
+	
+	print("Coef. de reflexão (T): {0}".format(TAU))
+	print("Coef. de transmissão (tau): {0}".format(tau))
+	print("Coef. de onda estacionária (VSWR): {0}".format(VSWR))
+	
